587959efe3bb23385eeda1be027bd4055179324a
dmitry <kovtunov.da@students.dvfu.ru>
2024-01-29 23:42:58 +1000

    add migrations

--- voting_system/voting/models.py
+++ voting_system/voting/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Users(models.Model):
+    full_name = models.CharField(max_length=256, blank=False, null=False)
+
+
+class Voting(models.Model):
+    variants = models.TextField(blank=False, null=False)
+
+
+class VotingProcess(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE)
+    voting = models.ForeignKey(Voting, on_delete=models.CASCADE)
+    enter_code = models.TextField(max_length=10, blank=False, null=False)
+    chosen = models.TextField(blank=False)
